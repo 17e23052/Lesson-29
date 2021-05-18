a2be584bbe8745def24ec960387b1c418bcd0b16
Mathman <>
2021-05-18 10:54:09 +0000

    10 Simon says with for loop

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+counter = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 from random import randint
 from time import sleep
 simon_says = ["Hands on head", "Hands on ears",
               "Right hand up", "Left hand up",
               "Hands on shoulders"]
-count = 0
-while count < 10:
+
+for counter in range(10):
   index = randint(0,4)
   instruction = simon_says[index]
-
   print(f"Simon says... {instruction}")
-  count = count + 1
   sleep(2)
